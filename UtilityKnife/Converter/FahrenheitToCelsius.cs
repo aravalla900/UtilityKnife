8e2a00016d70d4ef4d8c11b957d3f46400426fc6
aravalla900 <aravalla900@outlook.com>
2023-11-09 11:58:37 -0600

    create the class for FtoC converter

---
+++ UtilityKnife/Converter/FahrenheitToCelsius.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilityKnife.Converter
+{
+    public class FahrenheitToCelsius
+    {
+        public double FahrenheitToCelsiusConverter(double FahrenheitTemp)
+        {
+            double _fahrenheitTemp = 0.0;
+            double _celcius = 0.0;
+            return _celcius;
+        }
+    }
+}
